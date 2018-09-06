45669752c30cb0ed8de1d4d7a9a27c7c681a146d
gbalejandro <alejandrobright@gmail.com>
2018-09-06 08:51:42 -0500

    Se agregaron funciones al controlador Home.

--- IdentityDemo/Controllers/HomeController.cs
+++ IdentityDemo/Controllers/HomeController.cs
@@ -5,23 +5,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                var user = await userManager.GetUserAsync(HttpContext.User);
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
